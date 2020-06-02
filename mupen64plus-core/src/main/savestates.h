10b4c8a4ac4b380d4e31c19e2c54866f2dfaad62
Mahyar Koshkouei <mk@deltabeard.com>
2020-06-02 20:23:38 +0100

    mupen64: remove pj64 save state support
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- mupen64plus-core/src/main/savestates.h
+++ mupen64plus-core/src/main/savestates.h
@@ -36,8 +36,6 @@ typedef enum _savestates_type
 {
     savestates_type_unknown,
     savestates_type_m64p,
-    savestates_type_pj64_zip,
-    savestates_type_pj64_unc
 } savestates_type;
 
 savestates_job savestates_get_job(void);
